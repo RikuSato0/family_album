7b8886b56cabacb4090a8426f24d28ef286380cc
RikuSato0 <blockify41@gmail.com>
2025-06-06 11:54:16 +0300

    Init commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,16 @@
+const int noDbId = -9223372036854775808; // from Isar
+const double downloadCompleted = -1;
+const double downloadFailed = -2;
+
+// Number of log entries to retain on app start
+const int kLogTruncateLimit = 250;
+
+// Sync
+const int kSyncEventBatchSize = 5000;
+
+// Hash batch limits
+const int kBatchHashFileLimit = 128;
+const int kBatchHashSizeLimit = 1024 * 1024 * 1024; // 1GB
+
+// Secure storage keys
+const String kSecuredPinCode = "secured_pin_code";
---
+++ lib/constants/enums.dart
@@ -0,0 +1,12 @@
+enum SortOrder {
+  asc,
+  desc,
+}
+
+enum TextSearchType {
+  context,
+  filename,
+  description,
+}
+
+enum AssetVisibilityEnum { timeline, hidden, archive, locked }
